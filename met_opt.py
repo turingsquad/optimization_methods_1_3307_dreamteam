3ab7553255cad92b5e997a090eb7a6d61538a2da
jnannni <catya.ladokhina@gmail.com>
2019-11-01 13:20:01 +0300

    fibonacci

--- met_opt.py
+++ met_opt.py
@@ -35,3 +35,26 @@ def golden_section_method(a, b, eps=1e-4):
             b = x2
     return (a + b) / 2.0
 
+def fibonacci(n):
+    return int((((1 + math.sqrt(5)) / 2) ** n - ((1 - math.sqrt(5)) / 2) ** n) / math.sqrt(5))
+
+def fibonacci_method(a, b, e = 1e-4, i = 0):
+    while (a+b) / e  >= fibonacci(i):
+        i += 1
+    i -= 1
+    fib0 = fibonacci(i - 2)
+    fib1 = fibonacci(i - 1)
+    fib2 = fibonacci(i)
+    x1 = a + fib0 / fib2 * (b - a)
+    x2  = a + fib1 / fib2 * (b - a)
+    while b - a > e:    
+        x1 = a + fib0 / fib2 * (b - a)
+        x2  = a + fib1 / fib2 * (b - a)
+        if f(x1) < f(x2):
+            b = x2
+        elif f(x1) > f(x2):
+            a  = x1
+        else:
+            a = x1
+            b = x2
+    return (x1 + x2) / 2
