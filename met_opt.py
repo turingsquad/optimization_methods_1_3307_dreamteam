cdd37a247506d90db850a01c811043b45f52c24c
Ilya <ilyashin9@gmail.com>
2019-11-08 19:26:40 +0300

    add line search

--- met_opt.py
+++ met_opt.py
@@ -35,3 +35,47 @@ def golden_section_method(a, b, eps=1e-4):
             b = x2
     return (a + b) / 2.0
 
+def fibonacci(n):
+    return int((((1 + math.sqrt(5)) / 2) ** n - ((1 - math.sqrt(5)) / 2) ** n) / math.sqrt(5))
+
+def fibonacci_method(a, b, e = 1e-4, i = 0):
+    while (a+b) / e  >= fibonacci(i):
+        i += 1
+    i -= 1
+    fib0 = fibonacci(i - 2)
+    fib1 = fibonacci(i - 1)
+    fib2 = fibonacci(i)
+    x1 = a + fib0 / fib2 * (b - a)
+    x2  = a + fib1 / fib2 * (b - a)
+    while b - a > e:
+        x1 = a + fib0 / fib2 * (b - a)
+        x2  = a + fib1 / fib2 * (b - a)
+        if f(x1) < f(x2):
+            b = x2
+        elif f(x1) > f(x2):
+            a  = x1
+        else:
+            a = x1
+            b = x2
+    return (x1 + x2) / 2
+
+
+def line_search(x0, sigma):
+    x1 = 0
+    h = 0
+    if f(x0) > f(x0 + sigma):
+        x1 = x0 + sigma
+        h = sigma
+    elif f(x0) > f(x0 - sigma):
+        x1 = x0 - sigma
+        h = -sigma
+    h *= 2
+    x_k = x1
+    x_k1 = x_k + h  #xk+1
+    xk1 = x0        #xk-1
+    while f(x_k) > f(x_k1):
+        h *= 2
+        xk1 = x_k
+        x_k = x_k1
+        x_k1 = x_k + h
+    return xk1, x_k1
