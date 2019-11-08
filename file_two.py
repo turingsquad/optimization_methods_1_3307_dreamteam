c6d3e5a8b1055d15492d44d1c59deec38809d3d0
Ilya <ilyashin9@gmail.com>
2019-11-09 00:13:27 +0300

    remove golden section method from this task and add output for gradient

--- file_two.py
+++ file_two.py
@@ -1,9 +1,13 @@
 import math
 
+output_file = open("./output/file_two.txt", "w")
+
 def func(x):
+    global count
+    count += 1
     return (x[1] - x[0]**2)**2 + (1 - x[0])**2
 
-def num_gradient(f, x1, x2, h=1e-6):
+def num_gradient(f, x1, x2, h=1e-4):
     der_1 = (f([x1 + h, x2]) - f([x1 - h, x2])) / (2 * h)
     der_2 = (f([x1, x2 + h]) - f([x1, x2 - h])) / (2 * h)
     return [der_1, der_2]
@@ -18,7 +22,7 @@ def distance_between(x1, x2):
     for i in range(len(x1)):
         dist += (x2[i] - x1[i])**2
     dist = math.sqrt(dist)
-    return dist 
+    return dist
 
 def calculate_len(vector):
     length = 0
@@ -26,24 +30,6 @@ def calculate_len(vector):
         length += i ** 2
     length = math.sqrt(length)
     return length
-
-def golden_section_method(f, a, b, x, gradient, eps=1e-4):
-    x1_temp = [0, 0]
-    x2_temp = [0, 0]
-    while abs(b - a) > eps:
-        x1 = a + 0.381966011 * (b - a)
-        x2 = a + 0.618003399 * (b - a)
-        for i in range(len(x)):
-            x1_temp[i] = x[i] - x1 * gradient[i]
-            x2_temp[i] = x[i] - x2 * gradient[i]
-        if f(x1_temp) < f(x2_temp):
-            b = x1
-        elif f(x1_temp) > f(x2_temp):
-            a = x2
-        else:
-            a = x1
-            b = x2
-    return (a + b) / 2.0
 
 
 def dichotomy_method(f, a, b, x, gradient, eps=1e-4):
@@ -67,7 +53,7 @@ def dichotomy_method(f, a, b, x, gradient, eps=1e-4):
         mid = (b + a) / 2.0
     return mid
 
-def steepest_descent(f, x, gradient_func, eps=1e-2):
+def steepest_descent(f, x, gradient_func, eps=1e-4):
     x_prev = x.copy()
     x_new = [x_prev[0] + (eps * 2), x_prev[1] + (eps * 2)]
     i = 0
@@ -75,13 +61,16 @@ def steepest_descent(f, x, gradient_func, eps=1e-2):
         grad = gradient_func(f, x_prev[0], x_prev[1])
         grad_len = calculate_len(grad)
         grad = [grad[0]/grad_len, grad[1]/grad_len]
-        h = golden_section_method(f, 0, 100000, x_prev, grad)
+        h = dichotomy_method(f, 0, 100000, x_prev, grad)
         x_temp = x_new.copy()
         x_new = [x_prev[0] - h * grad[0], x_prev[1] - h * grad[1]]
         x_prev = x_temp
-        print("x_prev = " + str(x_prev) + " x_new = " + str(x_new) + " grad = " + str(grad))
         i += 1
+    output_file.write("iterations = " + str(i) + "\n")
     return x_new
 
-
-print(str(steepest_descent(func, [20, 20], num_gradient)))
+count = 0
+a = steepest_descent(func, [2, 2], an_gradient)
+output_file.write("coordinates = " + str(a) + "\n")
+output_file.write("function value = " + str(func(a)) + "\n")
+output_file.write("count of calling function = " + str(count) + "\n")
